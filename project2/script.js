2d6ab1229aa1c0c167e7e6c6aaec1e01e3a60283
Debanik98 <debaniksaha1234@gmail.com>
2022-07-11 14:21:14 +0530

    changes to script.js

---
+++ project2/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const modal = document.querySelectorAll(".show-modal");
+const hide = document.querySelectorAll(".hidden");
+const close = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+let showAll,closeAll;
+
+for(let i=0;i<modal.length;i++) {
+
+    modal[i].addEventListener('click', showAll = function() {
+        console.log("clicking")
+        for(let i=0;i<hide.length;i++) {
+
+            hide[i].style.display= 'block'
+
+        }})
+}
+
+close.addEventListener('click', closeAll = function(){
+    for(let i=0;i<hide.length;i++) {
+
+        hide[i].style.display= 'none'
+
+    }
+} )
+
+document.addEventListener('keydown',function(event){
+    if(event.key === 'Escape')
+    {
+        closeAll();
+    }})
+
+overlay.addEventListener('click', closeAll)
